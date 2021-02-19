d7dc93b2ebec48dc13080895d42a0bcf927a7da9
dev2006-sket <76101394+dev2006-sket@users.noreply.github.com>
2021-02-19 20:36:37 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,23 @@
+class Block extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.Visibiliy = 255;
+    }
+    display(){
+      console.log(this.body.speed);
+      if(this.body.speed < 3){
+        super.display();
+      } 
+      else{
+       World.remove(world, this.body);
+       push();
+       this.Visibility = this.Visibility - 5;
+       tint(255, this.Visibility);
+       pop();
+       
+      }
+ 
+     } 
+ }
+  
+  
